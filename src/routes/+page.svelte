d3fd28f9679deace4b272a898d35295232354f08
jjones <jimmy.jones@tutanota.com>
2023-10-08 14:17:39 -0600

    adjust bg color of separators

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,14 @@
 		<div class="flex-1 flex flex-col mr-6">
 			<div><Input type="text" placeholder="Search here" /></div>
 			<div class="flex items-center mt-4">
-				<div class="text-sm">List of eathquakes:</div>
-				<Separator class="flex-1 ml-2" decorative />
+				<div class="text-sm text-[#60656D]">List of eathquakes:</div>
+				<Separator class="flex-1 ml-2 bg-[#21272F]" decorative />
 			</div>
 			<!-- Lists of earthquakes-->
 			<div class="flex-1 overflow-scroll">list</div>
 			<div>compare list</div>
 		</div>
-		<Separator orientation="vertical" class="mr-4" decorative />
+		<Separator orientation="vertical" class="mr-4 bg-[#21272F]" decorative />
 	</div>
 	<div class="w-[596px] bg-[#303547]/20 border border-[#31373F] mb-4" />
 </div>

67e79489578ee43ecff97dacb3c0453fb9fca95c
jjones <jimmy.jones@tutanota.com>
2023-10-07 18:37:43 -0600

    rough header layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,45 @@
 <script>
 	import '../app.postcss';
+	import { Button } from '$lib/components/ui/button';
+	import { Toggle } from '$lib/components/ui/toggle';
+	import { Separator } from '$lib/components/ui/separator';
 </script>
 
+<header class="flex bg-[#0C1115] p-4 items-center">
+	<Button variant="default" size="sm">-</Button>
+	<div class="flex">
+		<span>img</span>
+		<h4>test</h4>
+	</div>
+	<div class="flex flex-1 justify-center">
+		<div class="bg-[#1F232F] rounded">
+			<Toggle size="sm">Overview</Toggle>
+			<Toggle size="sm">Real Time Activity</Toggle>
+			<Toggle size="sm">Prediction</Toggle>
+			<Toggle size="sm">History</Toggle>
+			<Toggle size="sm">Comparison</Toggle>
+		</div>
+	</div>
+	<div class="flex">
+		<div class="flex items-center">
+			<p class="font-bold text-[#E9DF9D] px-4">12</p>
+			<p class="text-xs leading-4">Number of<br /> Earthquakes</p>
+			<Separator orientation="vertical" class="bg-[#1D2125] ml-4" />
+		</div>
+		<div class="flex items-center">
+			<p class="font-bold text-[#E9DF9D] px-4">4</p>
+			<p class="text-xs leading-4">Average<br /> Frequency</p>
+			<Separator orientation="vertical" class="bg-[#1D2125] ml-4" />
+		</div>
+		<div class="flex items-center">
+			<p class="font-bold text-[#E9DF9D] px-4">6.0</p>
+			<p class="text-xs leading-4">Maximum<br /> Magnitude</p>
+			<Separator orientation="vertical" class="bg-[#1D2125] ml-4" />
+		</div>
+		<div class="flex items-center">
+			<p class="font-bold text-[#E9DF9D] px-4">6.0</p>
+			<p class="text-xs leading-4">Minimum<br /> Magnitude</p>
+		</div>
+	</div>
+</header>
 <slot />

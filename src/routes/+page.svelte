37584c911dcfeef44a6dda1f0c704a4e6e813cdf
jjones <jimmy.jones@tutanota.com>
2023-10-07 22:21:02 -0600

    add vertical seperator, fix right column bg color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator';
+</script>
+
 <div class="flex mt-4 ml-4 mr-4 flex-1">
-	<div class="flex-1">Hello</div>
-	<div class="w-[524px] bg-[#1A202C] border border-[#31373F] mb-4">Hello</div>
+	<div class="flex-1 flex">
+		<div class="flex-1">hello</div>
+		<Separator orientation="vertical" class="mr-4" />
+	</div>
+	<div class="w-[596px] bg-[#303547]/20 border border-[#31373F] mb-4" />
 </div>

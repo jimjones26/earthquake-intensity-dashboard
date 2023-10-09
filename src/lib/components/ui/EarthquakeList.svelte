c73fa9abbf94dd67765c48454cb535cb2619165f
jjones <jimmy.jones@tutanota.com>
2023-10-09 17:15:40 -0600

    refactor earthquake list into seperate component

---
+++ src/lib/components/ui/EarthquakeList.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Toggle } from '$lib/components/ui/toggle';
+	import { ChevronDown } from 'lucide-svelte';
+	import Intensity from '$lib/components/ui/Intensity.svelte';
+	import ItemRowData from '$lib/components/ui/ItemRowData.svelte';
+	export let items: any = [];
+</script>
+
+<div class="flex-1 overflow-scroll mt-2 mb-5">
+	<section class="flex flex-col">
+		{#each items as item}
+			<div class="mb-2 p-0 bg-[#4B4F5C]/25 h-12 cursor-pointer hover:bg-transparent mr-4 last:mb-0">
+				<div class="group flex-1 flex text-sm text-[#DADDE3]">
+					<Intensity intensity={item.intensity} />
+					<ItemRowData label="Time" value={item.time} columnWidthPercent="20" />
+					<ItemRowData label="Location" value={item.location} columnWidthPercent="30" />
+					<ItemRowData label="Coordinates" value={item.coordinates} columnWidthPercent="30" />
+					<ItemRowData label="Distanced" value={item.distanced} columnWidthPercent="10" />
+					<ItemRowData label="Depth" value={item.depth} columnWidthPercent="10" />
+					<Toggle class="h-12 w-12 rounded-none hover:bg-transparent"
+						><ChevronDown class="w-4 text-[#5C5F67]" /></Toggle
+					>
+				</div>
+			</div>
+		{/each}
+	</section>
+</div>

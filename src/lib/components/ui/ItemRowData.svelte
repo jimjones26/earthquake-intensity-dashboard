54ef69b84a99c333dd23b7a955e63f74bab2b941
jjones <jimmy.jones@tutanota.com>
2023-10-08 21:15:22 -0600

    potential fix for flex-basis not working

--- src/lib/components/ui/ItemRowData.svelte
+++ src/lib/components/ui/ItemRowData.svelte
@@ -4,6 +4,6 @@
 	export let columnWidthPercent: any;
 </script>
 
-<div class="flex flex-col basis-[{columnWidthPercent}%] items-start justify-center">
+<div style="flex-basis: {columnWidthPercent}%;" class="flex flex-col items-start justify-center">
 	<span class="text-[#6B6E76] text-xs">{label}</span><span>{value}</span>
 </div>

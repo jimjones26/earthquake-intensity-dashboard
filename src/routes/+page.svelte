bd059750bc739b3f80a3f2ced9896669ff3420e5
jjones <jimmy.jones@tutanota.com>
2023-10-08 20:46:59 -0600

    create intensity component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
 	import { Input } from '$lib/components/ui/input';
 	import { Toggle } from '$lib/components/ui/toggle';
 	import { ChevronDown } from 'lucide-svelte';
+	import Intensity from '$lib/components/ui/Intensity.svelte';
 
 	let earthquakes = [
 		{
 			id: 1,
-			intensity: '7.2',
+			intensity: 7.2,
 			time: '24h 11m',
 			location: 'Syracuse, Connecticut',
 			coordinates: '-7.67733, 19.16687',
@@ -16,12 +17,21 @@
 		},
 		{
 			id: 2,
-			intensity: '3.6',
+			intensity: 3.6,
 			time: '22h 01m',
 			location: '34th St W, Bradenton, USA',
 			coordinates: '28.96065, -123.11716',
 			distanced: '147 mi',
 			depth: '62 mi'
+		},
+		{
+			id: 3,
+			intensity: 1.6,
+			time: '11h 34m',
+			location: 'H.G. Suite 113, Wardville',
+			coordinates: '28.96065, -123.11716',
+			distanced: '12.8 mi',
+			depth: '1.22 mi'
 		}
 	];
 </script>
@@ -37,35 +47,26 @@
 			<!-- Lists of earthquakes-->
 			<div class="flex-1 overflow-scroll mt-2">
 				<section class="flex flex-col">
-					{#each earthquakes as earthquake}
+					{#each earthquakes as item}
 						<div class="mb-2 p-0 bg-[#4B4F5C]/25 h-12 cursor-pointer hover:bg-transparent">
-							<div class="flex-1 flex text-sm text-[#DADDE3]">
-								<!-- TODO:  make component-->
-								<div
-									class="flex bg-red-500 min-h-12 min-h-12 h-12 w-12 justify-center items-center mr-4"
-								>
-									{earthquake.intensity}
+							<div class="group flex-1 flex text-sm text-[#DADDE3]">
+								<Intensity intensity={item.intensity} />
+								<div class="flex flex-col basis-[20%] items-start justify-center">
+									<span class="text-[#6B6E76] text-xs">Time</span><span>{item.time}</span>
 								</div>
 								<div class="flex flex-col basis-[20%] items-start justify-center">
-									<span class="text-[#6B6E76] text-xs">Time</span><span>{earthquake.time}</span>
-								</div>
-								<div class="flex flex-col basis-[20%] items-start justify-center">
-									<span class="text-[#6B6E76] text-xs">Location</span><span
-										>{earthquake.location}</span
-									>
+									<span class="text-[#6B6E76] text-xs">Location</span><span>{item.location}</span>
 								</div>
 								<div class="flex flex-col basis-[20%] items-start justify-center">
 									<span class="text-[#6B6E76] text-xs">Coordinates</span><span
-										>{earthquake.coordinates}</span
+										>{item.coordinates}</span
 									>
 								</div>
 								<div class="flex flex-col basis-[20%] items-start justify-center">
-									<span class="text-[#6B6E76] text-xs">Distanced</span><span
-										>{earthquake.distanced}</span
-									>
+									<span class="text-[#6B6E76] text-xs">Distanced</span><span>{item.distanced}</span>
 								</div>
 								<div class="flex flex-col basis-[20%] items-start justify-center">
-									<span class="text-[#6B6E76] text-xs">Depth</span><span>{earthquake.depth}</span>
+									<span class="text-[#6B6E76] text-xs">Depth</span><span>{item.depth}</span>
 								</div>
 								<Toggle class="h-12 w-12"><ChevronDown /></Toggle>
 							</div>

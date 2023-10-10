1bc836532f459a0f8d85ab5635db8b39d5665224
jjones <jimmy.jones@tutanota.com>
2023-10-10 12:55:09 -0600

    changes needed to be able to restyle dragged element

--- src/lib/components/ui/EarthquakeList.svelte
+++ src/lib/components/ui/EarthquakeList.svelte
@@ -35,24 +35,25 @@
 		on:finalize={handleFinalize}
 	>
 		{#each items as item (item.id)}
-			<div
-				class="relative mb-2 p-0 bg-[#4B4F5C]/25 h-12 hover:bg-transparent mr-4 last:mb-0 transition ease-in-out duration-500 hover:transition hover:ease-in-out hover:duration-500 active:bg-[#0F141B]"
-				animate:flip={{ duration: flipDurationMs }}
-			>
-				<div class="group flex-1 flex text-sm text-[#DADDE3]">
-					<Intensity intensity={item.intensity} />
-					<ItemRowData label="Time" value={item.time} columnWidthPercent="20" />
-					<ItemRowData label="Location" value={item.location} columnWidthPercent="30" />
-					<ItemRowData label="Coordinates" value={item.coordinates} columnWidthPercent="30" />
-					<ItemRowData label="Distanced" value={item.distanced} columnWidthPercent="10" />
-					<ItemRowData label="Depth" value={item.depth} columnWidthPercent="10" />
-					<Toggle class="h-12 w-12 rounded-none hover:bg-transparent"
-						><ChevronDown class="w-4 text-[#5C5F67]" /></Toggle
-					>
+			<div class="mb-2 p-0 animate:flip={{ duration: flipDurationMs }}">
+				<div
+					class="relative bg-[#4B4F5C]/25 h-12 hover:bg-transparent mr-4 last:mb-0 transition ease-in-out duration-500 hover:transition hover:ease-in-out hover:duration-500 active:bg-[#0F141B] column"
+				>
+					<div class="group flex-1 flex text-sm text-[#DADDE3]">
+						<Intensity intensity={item.intensity} />
+						<ItemRowData label="Time" value={item.time} columnWidthPercent="20" />
+						<ItemRowData label="Location" value={item.location} columnWidthPercent="30" />
+						<ItemRowData label="Coordinates" value={item.coordinates} columnWidthPercent="30" />
+						<ItemRowData label="Distanced" value={item.distanced} columnWidthPercent="10" />
+						<ItemRowData label="Depth" value={item.depth} columnWidthPercent="10" />
+						<Toggle class="h-12 w-12 rounded-none hover:bg-transparent"
+							><ChevronDown class="w-4 text-[#5C5F67]" /></Toggle
+						>
+					</div>
+					{#if item[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
+						<div in:fade={{ duration: 500, easing: cubicIn }} class="custom-shadow-item" />
+					{/if}
 				</div>
-				{#if item[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
-					<div in:fade={{ duration: 500, easing: cubicIn }} class="custom-shadow-item" />
-				{/if}
 			</div>
 		{/each}
 	</section>
@@ -68,5 +69,8 @@
 		visibility: visible;
 		background-color: #1c233080;
 		background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23414650FF' stroke-width='2' stroke-dasharray='5%2c 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+	}
+	:global(#dnd-action-dragged-el .column) {
+		transform: rotate(7deg);
 	}
 </style>

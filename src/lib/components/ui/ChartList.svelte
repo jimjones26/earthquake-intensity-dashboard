c14978d06530b1c4fa9ba3cf6ef24b63843b0cb9
jjones <jimmy.jones@tutanota.com>
2023-10-10 13:20:14 -0600

    remove unused imports

--- src/lib/components/ui/ChartList.svelte
+++ src/lib/components/ui/ChartList.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import type { ListItem } from '$lib/ListItem';
-	import { Toggle } from '$lib/components/ui/toggle';
-	import { ChevronDown } from 'lucide-svelte';
-	import Intensity from '$lib/components/ui/Intensity.svelte';
-	import ItemRowData from '$lib/components/ui/ItemRowData.svelte';
 	import { dndzone, TRIGGERS, SOURCES, alertToScreenReader } from 'svelte-dnd-action';
-	import { flip } from 'svelte/animate';
 
 	export let items: ListItem[] = [];
 	export let type: string;

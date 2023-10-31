903fc0c3f107bc93befbb2ed89e23e4881433283
jjones <jimmy.jones@tutanota.com>
2023-10-30 19:54:48 -0600

    takes notes on required data from design

---
+++ src/lib/stores/interfaces.ts
@@ -0,0 +1,27 @@
+/*
+Design shows the following data:
+
+Upper right summary
+- Number of earthquakes
+- Average frequency
+- Maximum magnitude
+- Minimum magnitude
+
+List items
+- Magnitude
+- Location
+- Coordinates
+- Distanced
+- Depth
+
+Chart
+- Intensity, points
+- Magnitude, points
+- Depth, miles
+- Distance, miles
+- Buildings destroyed
+- Roads destroyed
+- People injured
+- People deaths
+*/
+

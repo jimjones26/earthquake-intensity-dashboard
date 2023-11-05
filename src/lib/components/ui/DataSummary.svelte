58d277eceb84eb56860684dd37ca1aad9230daa2
jjones <jimmy.jones@tutanota.com>
2023-11-04 21:35:36 -0600

    hide separator on last child

--- src/lib/components/ui/DataSummary.svelte
+++ src/lib/components/ui/DataSummary.svelte
@@ -5,11 +5,13 @@
 </script>
 
 <div class="flex">
-	{#each summaryData as summary}
+	{#each summaryData as summary, i}
 		<div class="flex items-center">
 			<p class="font-bold text-lg text-[#E9DF9D] px-4">{summary.value}</p>
 			<p class="text-sm font-bold leading-4 max-w-[95px]">{summary.label}</p>
-			<Separator orientation="vertical" class="bg-[#1D2125] ml-4 h-[90%]" />
+			{#if i !== summaryData.length - 1}
+				<Separator orientation="vertical" class="bg-[#1D2125] ml-4 h-[90%]" />
+			{/if}
 		</div>
 	{/each}
 </div>
